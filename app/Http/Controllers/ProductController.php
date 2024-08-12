43dbc56ff79e44ed700c0e3bd7c6ea350378a5b2
Sayed Abbas Fardan <sayedabbas20192873@gmail.com>
2024-08-12 18:28:42 +0300

    Initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\View\View;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('products.index', [
+			'products' => Product::with('user')->latest()->get(),
+		]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:100',
+			'description' => 'string|max:300',
+			'price' => 'required|numeric',
+        ]);
+ 
+        $request->user()->products()->create($validated);
+ 
+        return redirect(route('products.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product): View
+    {
+        Gate::authorize('update', $product);
+		
+		return view('products.edit', [
+			'product' => $product,
+		]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product): RedirectResponse
+    {
+        Gate::authorize('update', $product);
+ 
+        $validated = $request->validate([
+            'name' => 'required|string|max:100',
+			'description' => 'string|max:300',
+			'price' => 'required|numeric',
+        ]);
+ 
+        $product->update($validated);
+ 
+        return redirect(route('products.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product): RedirectResponse
+    {
+        Gate::authorize('delete', $product);
+ 
+        $product->delete();
+ 
+        return redirect(route('products.index'));
+    }
+}
